d060126b0b5911e112e02951f65a19a1a4b7d8ca
Evandro Calado <evandrocalado07@gmail.com>
2026-09-21 21:01:50 -0300

    feat: Add Tailwind CSS and shadcn UI support

--- admin-platform/src/App.tsx
+++ admin-platform/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
-    <div>
+    <div className="text-2xl font-bold underline text-primary">
       <h1>Admin Platform</h1>
+
+      <Button>Admin</Button>
     </div>
   );
 }
